ee7bbafc0abe65cf9f177c200d7fbc973815628e
FND <fnd@fnd-oda.(none)>
2011-03-26 12:16:01 +0100

    moved URI mappings to separate file

--- web/__init__.py
+++ web/__init__.py
@@ -2,13 +2,14 @@
 provides a WSGI application
 """
 
+import os
+
 from selector import Selector
 
 from web.middleware import UTF8_Encoder
 from web.handlers import get_root
 
 
-app = Selector()
-app.add('/', GET=get_root)
-
+mapfile = '%s/uris.map' % os.path.dirname(__file__) # XXX: use pkg_resources.resource_filename? (cf. tiddlyweb.config:URLS_MAP)
+app = Selector(mapfile=mapfile)
 app = UTF8_Encoder(app)
