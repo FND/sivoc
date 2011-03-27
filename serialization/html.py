107bd57fcaabf80be3d4ad9e59311c43e918c4dc
FND <fnd@fnd-oda.(none)>
2011-03-27 13:37:29 +0200

    introduced jinja2-based templating for HTML serialization

--- serialization/html.py
+++ serialization/html.py
@@ -1,28 +1,23 @@
-# TODO: embed in common layout
+import jinja2
+
+
+ENV = jinja2.Environment(loader=jinja2.FileSystemLoader('templates')) # XXX: use PackageLoader?
 
 
 def list_concepts(concepts):
-    yield '<ul>'
-    for concept in concepts:
-        label = concept.label()
-        uri = 'concepts/' + concept._id
-        yield '<li><a href="%s">%s</a></li>' % (uri, label)
-    yield '</ul>'
+
+    def template_data(concept):
+        return {
+            'uri': 'concepts/' + concept._id,
+            'label': concept.label()
+        }
+
+    concepts_data = (template_data(concept) for concept in concepts) # XXX: unnecessarily complex?
+
+    template = ENV.get_template('list_concepts.html')
+    return template.generate(concepts=concepts_data)
 
 
 def show_concept(concept): # TODO: i18n
-    yield '<dl>'
-
-    yield '<dt>ID</dt><dd>%s</dd>' % concept._id
-
-    line = '<dd>%s <span class="language">[%s]</span></dd>' # XXX: embedded SPAN less than ideal
-
-    yield '<dt>Preferred Labels</dt>'
-    for label in concept.pref_labels:
-        yield  line % (label.name, label.lang)
-
-    yield '<dt>Alternative Labels</dt>'
-    for label in concept.alt_labels:
-        yield line % (label.name, label.lang)
-
-    yield '</dl>'
+    template = ENV.get_template('show_concept.html')
+    return template.generate(concept=concept)
